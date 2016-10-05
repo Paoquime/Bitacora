e955afe2c8f8d759f6cc971ab0bf7a42e76f6687
Paoquime <paola.qmelendez@gmail.com>
2016-10-04 22:30:07 -0500

    click en post y desaparecen el resto de las opciones

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 window.addEventListener("load", main);
+  var post = document.getElementById("primeraOpcion");
+  var cita = document.getElementById("segundaOpcion");
+  var meme = document.getElementById("terceraOpcion");
+  var pastilla = document.getElementById("cuartaOpcion");
 
 function main() {
-  var post = document.getElementById("primeraOpcion");
-  post.addEventListener("click", addPost);
+  post.addEventListener("click", despliegue);
+  cita.addEventListener("click", despliegue);
+  meme.addEventListener("click", despliegue);
+  pastilla.addEventListener("click", despliegue);
 }
 
-function addPost (e){
-	e.preventDefault();
-	alert("hice click");
+function despliegue (){
+  cita.style.display = "none";
+  meme.style.display = "none";
+  pastilla.style.display = "none";
 }
-
