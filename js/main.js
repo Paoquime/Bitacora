c324dc4a7ffe56a63c7584a470d0e24d7833e559
Paoquime <paola.qmelendez@gmail.com>
2016-10-07 10:14:29 -0500

    agregando cambios en el css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//asigno cada id del html a una variable
 window.addEventListener("load", function(){
   var texto = document.getElementById("primeraOpcion");
   var cita = document.getElementById("segundaOpcion");
@@ -6,68 +5,57 @@ window.addEventListener("load", function(){
   var pastilla = document.getElementById("cuartaOpcion");
   var body = document.getElementsByTagName("body")[0];
 
-//cuando haga click en texto se ejecuta la función desplegar
-texto.addEventListener("click", function() {
-    desplegar();
-  });
 
-//creo los elementos que van a imprimirse en mi cajita-contenido
+var postArray = [texto, cita, meme, pastilla];
+
+for (elemento of postArray){
+    elemento.addEventListener("click", desplegar);
+}
+
 function desplegar() {
-    var contenido = document.createElement("div");
-    var title = document.createElement("input");
+	var contenido = document.createElement("div");
     var textArea = document.createElement("textarea");
     var publiBtn = document.createElement("button");
     var close = document.createElement("button");
-//les asigno un clase para poder modificarlos en el css
-    contenido.classList.add("contenido");
-    title.classList.add("title");
+
+	contenido.classList.add("contenido");
     textArea.classList.add("textArea");
     publiBtn.classList.add("publiBtn");
     close.classList.add("close");
-//agrego el texto a mis botones
+
     publiBtn.textContent = "Publicar";
-    close.textContent = "Cerrar";
-//les asigno la posición que van a tener
+    close.textContent = "Cerrar";    
+
     body.insertBefore(contenido, body.children[2]);
-    contenido.insertBefore(title, contenido.children[0]);
-    contenido.insertBefore(textArea, contenido.children[1]);
-    contenido.insertBefore(publiBtn, contenido.children[2]);
-    contenido.insertBefore(close, contenido.children[3]);
-//a mi boton close le asigno el evento addEventListener y le agrego el metodo
-//remove para que desaparezca en caso el usuario cambie de opinión.
+    contenido.insertBefore(textArea, contenido.children[0]);
+    contenido.insertBefore(publiBtn, contenido.children[1]);
+    contenido.insertBefore(close, contenido.children[2]);
+
     close.addEventListener("click", function() {
       contenido.remove();
     });
-//a mi boton publiBtn le asigno el evento addEventListener, 
-//creo tres div que contendran el valor que ingrese en el titulo y textarea 
+
     publiBtn.addEventListener("click", function() {
       var imprimir = document.createElement("div");
-      var titleImpre = document.createElement("div");
       var textImpre = document.createElement("div");
-//le asigno el valor ingresado por el usuario a mi div de titleImpre y  textImpre respectivamente
-    titleImpre.innerHTML = title.value;
+
     textImpre.innerHTML = textArea.value;
-//les asigno un clase para poder modificarlos en el css
-    imprimir.classList.add("contenido");
-    titleImpre.classList.add("titulo");
+
+    imprimir.classList.add("contenidoDos");
     textImpre.classList.add("escrito");
-//les asigno la posición que van a tener
+
     body.insertBefore(imprimir, body.children[3]);
-    imprimir.insertBefore(titleImpre, imprimir.children[0]);
-    imprimir.insertBefore(textImpre, imprimir.children[1]);
-//agrego las variables que contendran la hora de la publicación y le coloco la posición en la que va a estar
+    imprimir.insertBefore(textImpre, imprimir.children[0]);
+
     var fecha = new Date();
     var hora = fecha.getHours();
     var min = fecha.getMinutes();
     var almacenaHora = document.createElement("div");
-    almacenaHora.innerText = hora + ":" + min;
-    imprimir.insertBefore(almacenaHora, imprimir.children[2]); 
+     almacenaHora.classList.add("hora");
+    almacenaHora.innerText = hora + ":" + min + " " + "am";
+    imprimir.insertBefore(almacenaHora, imprimir.children[1]);
 
     });
   }
 
-}); 
-
-
-
-
+});
